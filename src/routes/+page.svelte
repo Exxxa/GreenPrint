cc38b7a83e25d03a97c2dfec22a36b1fa47caf6d
Hadi <112569860+anotherhadi@users.noreply.github.com>
2023-11-26 20:18:39 +0100

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h2>Liens:</h2>
+<p>
+  <br><a href="https://www.skeleton.dev/docs/introduction">SkeletonUI doc</a>
+  <br><a href="https://svelte.dev/docs/introduction">Svelte Doc</a>
+  <br><a href="https://learn.svelte.dev/tutorial/welcome-to-svelte">Exercice pour apprendre Svelte</a>
+  <br><a href="https://tailwindcss.com/docs/installation">Tailwindcss doc</a>
+</p>
