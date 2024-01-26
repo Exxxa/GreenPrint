cf18385a86e9ee5fd02e0fa4468731b99938fcff
drissji <125646775+drissji@users.noreply.github.com>
2024-01-26 15:51:06 +0100

    added navbar + how page is responsive + created a simple side bar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
+	import Sidebar from './sidebar/+page.svelte';
+  
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
-</script>
-
-<!-- App Shell -->
-<AppShell>
+  </script>
+  
+  <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">GreenPrint</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-        <p>Link</p>
-			</svelte:fragment>
-		</AppBar>
+	  <AppBar>
+		<svelte:fragment slot="lead">
+		  <button class="text-white">
+			&#9776; Menu
+		  </button>
+		</svelte:fragment>
+		<strong class="text-xl uppercase flex items-center justify-center">GreenPrint</strong> 
+		<svelte:fragment slot="trail">
+		  <button class="text-white">Login</button> 
+		  <button class="text-white border border-green-500 hover:bg-green-500 hover:text-white px-4 py-2 rounded">Sign Up</button>
+		</svelte:fragment>
+	  </AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
 	<slot />
-</AppShell>
+  </AppShell>
+  
