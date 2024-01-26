7a3bacdcba9c7191cf19913600c8ebb0c65caf59
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-01-26 14:49:12 +0100

    Composant card

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,4 +1,17 @@
-<div>
-    <h2 class="text-3xl">Card Title</h2>
-    <p><slot></slot></p>
+<script lang="ts">
+	export let title: string = 'Card title';
+	export let description: string =
+		'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.';
+	export let image: string =
+		'https://www.pixelstalk.net/wp-content/uploads/2016/11/Nature-Images-Foggy-Forest.jpg';
+	let alt: string = title + ' image';
+</script>
+
+<div class="card card-hover rounded-t-md w-80">
+	<img src={image} {alt} class="w-full rounded-t-md" />
+	<h2 class="text-2xl p-5">{title}</h2>
+
+	<div class="footer p-5">
+		<p>{description}</p>
+	</div>
 </div>
