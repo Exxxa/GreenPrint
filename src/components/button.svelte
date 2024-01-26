be2b57399dd38b6724a6fb29e343f7c32894f9e2
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-01-26 15:08:32 +0100

    Composant button

---
+++ src/components/button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { mg } from '../lib/utils';
+
+	export let size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
+</script>
+
+<button
+	class={mg(
+		'btn',
+		size === 'sm' && 'btn-sm',
+		size === 'md' && 'btn-md',
+		size === 'lg' && 'btn-lg',
+		size === 'xl' && 'btn-xl'
+	)}
+>
+	<slot />
+</button>
