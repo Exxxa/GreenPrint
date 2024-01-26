8bede7ba8d95e137afdb359fb5d7352822bab9bf
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-01-26 15:11:06 +0100

    Add link button

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -2,16 +2,19 @@
 	import { mg } from '../lib/utils';
 
 	export let size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
+	export let link: string = '#';
 </script>
 
-<button
-	class={mg(
-		'btn',
-		size === 'sm' && 'btn-sm',
-		size === 'md' && 'btn-md',
-		size === 'lg' && 'btn-lg',
-		size === 'xl' && 'btn-xl'
-	)}
->
-	<slot />
-</button>
+<a href={link}>
+	<button
+		class={mg(
+			'btn',
+			size === 'sm' && 'btn-sm',
+			size === 'md' && 'btn-md',
+			size === 'lg' && 'btn-lg',
+			size === 'xl' && 'btn-xl'
+		)}
+	>
+		<slot />
+	</button>
+</a>
