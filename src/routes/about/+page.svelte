d73f8510c4d78fc816eafef8d630cdbd2896d9e3
cduval <cduval@et.esiea.fr>
2024-01-27 16:58:53 +0100

    ajout about us

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,60 @@
+<div class="text-center">
+    <h1 class="text-3xl font-bold">A propos de Greenprint</h1>
+</div>
+
+<div class="flex flex-col h-2/5 items-center justify-center gap-5 md:flex-row">
+	<div class="text-center">
+		<p class="text-xl">Qui sommes nous ?</p>
+		<p>L’Organisation des Nations Unies est une organisation internationale fondée en 1945. Aujourd’hui, elle compte 193 États Membres. La mission et le travail des Nations Unies sont guidés par les objectifs et principes énoncés par sa Charte fondatrice.</p>
+	</div>
+	<img src="undraw_solution_mindset_re_57bf.svg" alt="t" class="md:w-80" />
+</div>
+
+<p class="text-2xl text-center">Nos convictions</p>
+<p class="mx-auto my-8 text-xl text-center max-w-lg">
+	Bienvenue sur Greenprint, où l'innovation rencontre la durabilité. Un écosystème numérique dédié à
+	la création d'un impact positif dans le monde
+</p>
+
+<div class="flex flex-col h-2/5 items-center justify-center gap-10 md:flex-row">
+	<div class="text-center md:w-1/2">
+		<p class="text-2xl">Façonnez l'avenir avec vos compétences uniques</p>
+		<p class="text-2xl">Votre savoir-faire, un catalyseur pour un avenir durable</p>
+		<p class="text-2xl">Cultivez un avenir durable avec vos compétences exceptionnelles</p>
+	</div>
+</div>
+
+<h3 class="text-3xl font-bold my-10 text-center">Pourquoi choisir Greenprint</h3>
+
+<div class="flex flex-col items-center justify-center gap-10 md:flex-row">
+	<div class="md:w-1/2 flex flex-wrap gap-5">
+		<div>
+			<h3 class="text-2xl font-bold">Notre engagement envers le changement</h3>
+			<p class="text-xl">
+				Une plateforme dédiée exclusivement aux projets liés au développement durable
+			</p>
+			<p class="text-xl">
+				Nous croyons que chaque ligne de code peut contribuer à un avenir plus durable
+			</p>
+		</div>
+		<div>
+			<h3 class="text-2xl font-bold">La puissance de la collaboration</h3>
+			<p class="text-xl">
+				Rencontrez des porteurs de projets passionnés et des programmeurs talentueux
+			</p>
+			<p class="text-xl">
+				Collaborez avec une communauté engagée dans la création d'innovations durables
+			</p>
+		</div>
+		<div>
+			<h3 class="text-2xl font-bold">Diversité des opportunités</h3>
+			<p class="text-xl">
+				Explorez une variété de projets allant de l'énergie propre à la conservation de la
+				biodiversité
+			</p>
+			<p class="text-xl">Des opportunités pour tous les domaines du développement durable</p>
+		</div>
+	</div>
+</div>
+
+<div class="pb-96"></div>
