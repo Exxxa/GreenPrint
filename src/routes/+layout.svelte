914221d0514bc2c22bb3161651ffef49af90527d
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-01-27 09:50:41 +0100

    Change navbar & add logo components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,94 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-  
+	import {
+		AppShell,
+		AppBar,
+		type DrawerSettings,
+		getDrawerStore,
+		Drawer,
+		TreeView,
+		TreeViewItem
+	} from '@skeletonlabs/skeleton';
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	import Logo from '../components/logo.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
-  </script>
-  
-  <!-- App Shell -->
-  <AppShell>
+
+	initializeStores();
+
+	const drawerSettings: DrawerSettings = {
+		id: 'menu',
+		bgDrawer: 'bg-surface-600 text-white',
+		bgBackdrop: 'bg-gradient-to-tr from-surface-400/60 to-surface-400/60',
+		width: 'w-[280px] md:w-[480px]',
+		padding: 'p-4',
+		rounded: 'rounded-xl'
+	};
+	const drawerStore = getDrawerStore();
+	function opennav() {
+		drawerStore.open(drawerSettings);
+	}
+	// function closenav() {
+	// 	drawerStore.close(drawerSettings);
+	// }
+</script>
+
+<Drawer>
+	<div class="p-10 flex flex-col gap-10">
+		<div class="flex gap-5 item-center justify-center items-center">
+			<Logo width={50} height={50} />
+			<strong class="text-xl uppercase flex item-center justify-center">GreenPrint</strong>
+		</div>
+		<TreeView>
+			<TreeViewItem>
+				Accueil
+				<svelte:fragment slot="children">
+					<TreeViewItem>
+						(Child 1)
+						<svelte:fragment slot="children">
+							<TreeViewItem>(Child of Child 1)</TreeViewItem>
+							<TreeViewItem>(Child of Child 2)</TreeViewItem>
+						</svelte:fragment>
+					</TreeViewItem>
+					<TreeViewItem>(Child 2)</TreeViewItem>
+				</svelte:fragment>
+			</TreeViewItem>
+			<TreeViewItem>A propos de nous</TreeViewItem>
+			<TreeViewItem>Les projets</TreeViewItem>
+			<TreeViewItem>Les Objectifs du développement durable</TreeViewItem>
+			<TreeViewItem>Comment contribuer ?</TreeViewItem>
+		</TreeView>
+	</div>
+</Drawer>
+<!-- App Shell -->
+<AppShell>
 	<svelte:fragment slot="header">
-	  <!-- App Bar -->
-	  <AppBar>
-		<svelte:fragment slot="lead">
-		  <!-- Menu Button (on the left) -->
-		  <button class="text-white">
-			<!-- You can use an icon or any symbol for the menu -->
-			&#9776; Menu
-		  </button>
-		</svelte:fragment>
-		<strong class="text-xl uppercase flex item-center justify-center">GreenPrint</strong> 
-		<svelte:fragment slot="trail">
-		  <!-- Login/Sign-up Buttons (on the right) -->
-		  <button class="text-white">Login</button>
-		  <button class="text-white border border-green-500 hover:bg-green-500 hover:text-white px-4 py-2 rounded">Sign Up</button>
-		</svelte:fragment>
-	  </AppBar>
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<!-- Menu Button (on the left) -->
+				<button class="btn variant-ghost" on:click={opennav}> &#9776; Menu </button>
+			</svelte:fragment>
+			<div class="flex gap-5 item-center justify-center items-center">
+				<Logo width={50} height={50} />
+				<strong class="text-xl uppercase flex item-center justify-center">GreenPrint</strong>
+			</div>
+			<svelte:fragment slot="trail">
+				<!-- Login/Sign-up Buttons (on the right) -->
+				<a href="/login">
+					<button class="btn variant-outline">Login</button>
+				</a>
+				<a href="/signup">
+					<button class="btn variant-soft-primary">Sign Up</button>
+				</a>
+			</svelte:fragment>
+		</AppBar>
 	</svelte:fragment>
 
 	<!-- Page Route Content -->
 	<slot />
-  </AppShell>
-  
+</AppShell>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Greenprint</title>
-    <link href="path/to/tailwind.css" rel="stylesheet"> <!-- Add the correct path to your Tailwind CSS file -->
-    <!-- Add any other stylesheets or meta tags as needed -->
-</head>
-<body class="bg-gray-100">
+<div class="flex flex-col items-center justify-center gap-5 md:flex-row">
+	<div class="text-center">
+		<h1 class="text-3xl font-bold">Construisons ensemble</h1>
+		<p class="text-xl">Un monde plus durable comme ici</p>
+		<button type="button" class="m-5 btn rounded-md variant-filled">Rejoignez Nous</button>
+	</div>
+	<div class="text-center">
+		<img src="undraw_sharing_knowledge_03vp.svg" class="md:w-96" />
+		<p class="m-4 text-2xl text-center w-96">
+			Bienvenue sur Greenprint, où l'innovation rencontre la durabilité. Un écosystème numérique
+			dédié à la création d'un impact positif dans le monde
+		</p>
+	</div>
+</div>
 
-  <div class="flex flex-col items-center justify-center gap-5 md:flex-row">
-    <div class="text-center">
-      <h1 class="text-3xl font-bold">Construisons ensemble</h1>
-      <p class="text-xl">Un monde plus durable comme ici</p>
-      <button type="button" class="m-5 btn rounded-md variant-filled">Rejoignez Nous</button>
-    </div>
-    <div class="text-center">
-      <img src="undraw_sharing_knowledge_03vp.svg" class="md:w-96">
-      <p class="m-4 text-2xl text-center w-96">Bienvenue sur Greenprint, où l'innovation rencontre la durabilité. Un écosystème numérique dédié à la création d'un impact positif dans le monde</p>
-    </div>
-  </div>
+<div class="flex flex-col items-center justify-center gap-10 md:flex-row">
+	<div class="text-center md:w-1/2">
+		<p class="text-2xl">Façonnez l'avenir avec vos compétences uniques</p>
+		<p class="text-2xl">Votre savoir-faire, un catalyseur pour un avenir durable</p>
+		<p class="text-2xl">Cultivez un avenir durable avec vos compétences exceptionnelles</p>
+	</div>
+	<img src="undraw_solution_mindset_re_57bf.svg" alt="second" class="w-full md:w-1/3" />
+</div>
 
-  <div class="flex flex-col items-center justify-center gap-10 md:flex-row">
-    <div class="text-center md:w-1/2">
-      <p class="text-2xl">Façonnez l'avenir avec vos compétences uniques</p>
-      <p class="text-2xl">Votre savoir-faire, un catalyseur pour un avenir durable</p>
-      <p class="text-2xl">Cultivez un avenir durable avec vos compétences exceptionnelles</p>
-    </div>
-    <img src="undraw_solution_mindset_re_57bf.svg" class="w-full md:w-1/3">
-  </div>
+<h3 class="text-3xl font-bold my-10 text-center">Pourquoi choisir Greenprint</h3>
 
-  <h3 class="text-3xl font-bold my-10 text-center">Pourquoi choisir Greenprint</h3>
-
-  <div class="flex flex-col items-center justify-center gap-10 md:flex-row">
-    <img src="undraw_online_test_re_kyfx.svg" class="w-full md:w-1/3">
-    <div class="text-center md:w-1/2">
-      <div>
-        <h3 class="text-2xl font-bold">Notre engagement envers le changement</h3>
-        <p class="text-xl">Une plateforme dédiée exclusivement aux projets liés au développement durable</p>
-        <p class="text-xl">Nous croyons que chaque ligne de code peut contribuer à un avenir plus durable</p>
-      </div>
-      <div>
-        <h3 class="text-2xl font-bold">La puissance de la collaboration</h3>
-        <p class="text-xl">Rencontrez des porteurs de projets passionnés et des programmeurs talentueux</p>
-        <p class="text-xl">Collaborez avec une communauté engagée dans la création d'innovations durables</p>
-      </div>
-      <div>
-        <h3 class="text-2xl font-bold">Diversité des opportunités</h3>
-        <p class="text-xl">Explorez une variété de projets allant de l'énergie propre à la conservation de la biodiversité</p>
-        <p class="text-xl">Des opportunités pour tous les domaines du développement durable</p>
-      </div>
-    </div>
-  </div>
-
-</body>
-</html>
+<div class="flex flex-col items-center justify-center gap-10 md:flex-row">
+	<img src="undraw_online_test_re_kyfx.svg" alt="third" class="w-full md:w-1/3" />
+	<div class="text-center md:w-1/2">
+		<div>
+			<h3 class="text-2xl font-bold">Notre engagement envers le changement</h3>
+			<p class="text-xl">
+				Une plateforme dédiée exclusivement aux projets liés au développement durable
+			</p>
+			<p class="text-xl">
+				Nous croyons que chaque ligne de code peut contribuer à un avenir plus durable
+			</p>
+		</div>
+		<div>
+			<h3 class="text-2xl font-bold">La puissance de la collaboration</h3>
+			<p class="text-xl">
+				Rencontrez des porteurs de projets passionnés et des programmeurs talentueux
+			</p>
+			<p class="text-xl">
+				Collaborez avec une communauté engagée dans la création d'innovations durables
+			</p>
+		</div>
+		<div>
+			<h3 class="text-2xl font-bold">Diversité des opportunités</h3>
+			<p class="text-xl">
+				Explorez une variété de projets allant de l'énergie propre à la conservation de la
+				biodiversité
+			</p>
+			<p class="text-xl">Des opportunités pour tous les domaines du développement durable</p>
+		</div>
+	</div>
+</div>
