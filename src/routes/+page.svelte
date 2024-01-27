ab51b35487c3c368f9c99d2051c34124366d9ad5
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-01-27 13:55:40 +0100

    Fix espacement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,31 @@
-<div class="flex flex-col items-center justify-center gap-5 md:flex-row">
+<div class="flex flex-col h-2/5 items-center justify-center gap-5 md:flex-row">
 	<div class="text-center">
 		<h1 class="text-3xl font-bold">Construisons ensemble</h1>
 		<p class="text-xl">Un monde plus durable comme ici</p>
 		<button type="button" class="m-5 btn rounded-md variant-filled">Rejoignez Nous</button>
 	</div>
-	<div class="text-center">
-		<img src="undraw_sharing_knowledge_03vp.svg" class="md:w-96" />
-		<p class="m-4 text-2xl text-center w-96">
-			Bienvenue sur Greenprint, où l'innovation rencontre la durabilité. Un écosystème numérique
-			dédié à la création d'un impact positif dans le monde
-		</p>
-	</div>
+	<img src="undraw_sharing_knowledge_03vp.svg" alt="t" class="md:w-96" />
 </div>
 
-<div class="flex flex-col items-center justify-center gap-10 md:flex-row">
+<p class="mx-auto my-8 text-2xl text-center max-w-lg">
+	Bienvenue sur Greenprint, où l'innovation rencontre la durabilité. Un écosystème numérique dédié à
+	la création d'un impact positif dans le monde
+</p>
+
+<div class="flex flex-col h-2/5 items-center justify-center gap-10 md:flex-row">
+	<img src="undraw_solution_mindset_re_57bf.svg" alt="second" class="w-full md:w-1/3" />
 	<div class="text-center md:w-1/2">
 		<p class="text-2xl">Façonnez l'avenir avec vos compétences uniques</p>
 		<p class="text-2xl">Votre savoir-faire, un catalyseur pour un avenir durable</p>
 		<p class="text-2xl">Cultivez un avenir durable avec vos compétences exceptionnelles</p>
 	</div>
-	<img src="undraw_solution_mindset_re_57bf.svg" alt="second" class="w-full md:w-1/3" />
 </div>
 
 <h3 class="text-3xl font-bold my-10 text-center">Pourquoi choisir Greenprint</h3>
 
 <div class="flex flex-col items-center justify-center gap-10 md:flex-row">
 	<img src="undraw_online_test_re_kyfx.svg" alt="third" class="w-full md:w-1/3" />
-	<div class="text-center md:w-1/2">
+	<div class="md:w-1/2 flex flex-wrap gap-5">
 		<div>
 			<h3 class="text-2xl font-bold">Notre engagement envers le changement</h3>
 			<p class="text-xl">
@@ -55,3 +54,5 @@
 		</div>
 	</div>
 </div>
+
+<div class="pb-96"></div>
